5df17556a72092081550e1fb02ccb98540584389
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2018-12-04 23:07:13 +0200

    Small late night fixes.
    Hopefully I didn't break anything...

--- Index/javascript/Calculator.js
+++ Index/javascript/Calculator.js
@@ -1,4 +1,27 @@
-function calculator() {
-  
+//This small piece of JavaScript will take user input and convert it to Hex,Dec,Bin and Oct values.
+
+
+
+
+
+
+
+
+
+
+function convHEX() {
+  var user_input =
+
+}
+function convDEC() {
+  var user_input =
+
+}
+function convBIN() {
+  var user_input =
+
+}
+function convOCT() {
+  var user_input =
 
 }
