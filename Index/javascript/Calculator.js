a98cdb14e9dbcd61f0430d0aacf5997321f0e017
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2018-12-05 10:33:58 +0200

    Starting fresh with JS

--- Index/javascript/Calculator.js
+++ Index/javascript/Calculator.js
@@ -1,27 +1,5 @@
-//This small piece of JavaScript will take user input and convert it to Hex,Dec,Bin and Oct values.
+// This small piece of JavaScript will take user input and
+// convert it to Hex,Dec,Bin and Oct values.
 
 
-
-
-
-
-
-
-
-
-function convHEX() {
-  var user_input =
-
-}
-function convDEC() {
-  var user_input =
-
-}
-function convBIN() {
-  var user_input =
-
-}
-function convOCT() {
-  var user_input =
-
-}
+function 
