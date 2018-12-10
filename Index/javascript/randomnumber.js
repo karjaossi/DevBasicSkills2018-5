5d3acf1739293f64b4659483b1f2ff5535bcef42
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2018-12-10 09:46:25 +0200

    Trying to work to get the canvas working

--- Index/javascript/randomnumber.js
+++ Index/javascript/randomnumber.js
@@ -1,3 +1,6 @@
+
+
+
 function randomnumber() {
 	var results = [];
 	for (var i = 0; i < 40; i++) {
@@ -16,14 +19,32 @@ function randomnumber() {
 		array.forEach((v) => (v === value && count++));
 		return count;
 	}
-  document.getElementById('RNGoutput1').innerHTML = getOccurrence(results, 1);
-  document.getElementById('RNGoutput2').innerHTML = getOccurrence(results, 2);
-  document.getElementById('RNGoutput3').innerHTML = getOccurrence(results, 3);
-  document.getElementById('RNGoutput4').innerHTML = getOccurrence(results, 4);
-  document.getElementById('RNGoutput5').innerHTML = getOccurrence(results, 5);
-  document.getElementById('RNGoutput6').innerHTML = getOccurrence(results, 6);
-  document.getElementById('RNGoutput7').innerHTML = getOccurrence(results, 7);
-  document.getElementById('RNGoutput8').innerHTML = getOccurrence(results, 8);
-  document.getElementById('RNGoutput9').innerHTML = getOccurrence(results, 9);
-  document.getElementById('RNGoutput10').innerHTML = getOccurrence(results, 10);
+
+	document.getElementById('RNGoutput1').innerHTML = getOccurrence(
+		results,
+		1
+	);
+
+		var c = document.getElementById("RNGblock1");
+		var xyx = getOccurrence(results, 1);
+		var ctx = c.getContext("2d");
+			ctx.fillStyle = "#FF0000";
+			ctx.fillRect(20, 20, 25, (xyx *25));
+
+	}
+	
+
+
+function reset() {
+
+	if (document.getElementById("RNGcolumn").style.display == "flex") {
+	document.getElementById("RNGcolumn").style.display = "none";
+
+	} else {
+		{
+			document.getElementById("RNGcolumn").style.display = "flex";
+
+		}
+	}
+
 }
